dc1f45fea4dc7c5d282e1313ff52778dfc41ca9b
Лизин Александр Сергеевич <lizin_as@tomsk.lemz.ru>
2016-12-20 02:34:58 +0700

    * Class Render was changed. Method Render::glLoad changed to be static. Method Render::glLoad produce instance of class Render if context created * Method Render::setBackgroundColor added * Errors handling added

--- lib/src/render.cpp
+++ lib/src/render.cpp
@@ -4,9 +4,61 @@
 
 using namespace glRender;
 
+void APIENTRY glDebugOutput(uint source,
+                            uint type,
+                            uint id,
+                            uint severity,
+                            int length,
+                            const char * message,
+                            const void * userParam)
+{
+    // ignore non-significant error/warning codes
+    if(id == 131169 || id == 131185 || id == 131218 || id == 131204) return;
+
+    std::cout << "---------------" << std::endl;
+    std::cout << "Debug message (" << id << "): " <<  message << std::endl;
+
+    switch (source)
+    {
+        case GL_DEBUG_SOURCE_API:             std::cout << "Source: API"; break;
+        case GL_DEBUG_SOURCE_WINDOW_SYSTEM:   std::cout << "Source: Window System"; break;
+        case GL_DEBUG_SOURCE_SHADER_COMPILER: std::cout << "Source: Shader Compiler"; break;
+        case GL_DEBUG_SOURCE_THIRD_PARTY:     std::cout << "Source: Third Party"; break;
+        case GL_DEBUG_SOURCE_APPLICATION:     std::cout << "Source: Application"; break;
+        case GL_DEBUG_SOURCE_OTHER:           std::cout << "Source: Other"; break;
+    } std::cout << std::endl;
+
+    switch (type)
+    {
+        case GL_DEBUG_TYPE_ERROR:               std::cout << "Type: Error"; break;
+        case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: std::cout << "Type: Deprecated Behaviour"; break;
+        case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:  std::cout << "Type: Undefined Behaviour"; break;
+        case GL_DEBUG_TYPE_PORTABILITY:         std::cout << "Type: Portability"; break;
+        case GL_DEBUG_TYPE_PERFORMANCE:         std::cout << "Type: Performance"; break;
+        case GL_DEBUG_TYPE_MARKER:              std::cout << "Type: Marker"; break;
+        case GL_DEBUG_TYPE_PUSH_GROUP:          std::cout << "Type: Push Group"; break;
+        case GL_DEBUG_TYPE_POP_GROUP:           std::cout << "Type: Pop Group"; break;
+        case GL_DEBUG_TYPE_OTHER:               std::cout << "Type: Other"; break;
+    } std::cout << std::endl;
+
+    switch (severity)
+    {
+        case GL_DEBUG_SEVERITY_HIGH:         std::cout << "Severity: high"; break;
+        case GL_DEBUG_SEVERITY_MEDIUM:       std::cout << "Severity: medium"; break;
+        case GL_DEBUG_SEVERITY_LOW:          std::cout << "Severity: low"; break;
+        case GL_DEBUG_SEVERITY_NOTIFICATION: std::cout << "Severity: notification"; break;
+    } std::cout << std::endl;
+    std::cout << std::endl;
+}
+
+Render::Render()
+{
+
+}
+
 void Render::draw(Scene *scene)
 {
-    glClearColor ( 0.5, 0.5, 0.5, 1.0 );
+    glClearColor ( m_background.x, m_background.y, m_background.z, m_background.w );
     glEnable     ( GL_DEPTH_TEST );
     glEnable     ( GL_BLEND);
     glDepthFunc  ( GL_LEQUAL );
@@ -16,14 +68,31 @@ void Render::draw(Scene *scene)
     scene->draw();
 }
 
-void Render::setViewPortSize(int w, int h)
+void Render::setViewportSize(const int & w, const int & h)
 {
-    glViewport ( 0, 0, (GLsizei)w, (GLsizei)h );
+    glViewport(0, 0, w, h);
 }
 
-bool Render::glLoad()
+void Render::setBackgroundColor(const Vec4 &background)
 {
-    return gladLoadGL();
+    m_background = background;
+}
+
+Render * Render::glLoad()
+{
+    if (gladLoadGL())
+    {
+        if (GL_CONTEXT_FLAG_DEBUG_BIT)
+        {
+            glEnable(GL_DEBUG_OUTPUT);
+            glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+            glDebugMessageCallback(glDebugOutput, nullptr);
+            glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, nullptr, GL_TRUE);
+        }
+
+        return new Render();
+    }
+    return nullptr;
 }
 
 const char * getGLString(uint key)
