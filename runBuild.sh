0bf7113fa8e6d6068845d9233a9592dca971b82b
Лизин Александр Сергеевич <lizin_as@tomsk.lemz.ru>
2017-02-19 23:14:03 +0700

    Add ability deploy generated packages with runBuild.sh. To deploy packages to sogimu.fvds.ru run runBuild.sh with env vars GLRENDER_DEPLOY_PACKAGES=ON GLRENDER_SLAVE_NAME=/*os name where build happend*/

--- runBuild.sh
+++ runBuild.sh
@@ -9,7 +9,7 @@ make -j 2
 
 ./tests/tests
 
-make package_source
+# make package_source
 make package
 
 #mkdir -p ~/glRender/builds/${NODE_NAME}/"build-Arch=${Arch}WithTests=${WithTests}"/${BUILD_ID}
@@ -17,16 +17,38 @@ make package
 #cp glRender*.deb ~/glRender/builds/${NODE_NAME}/"build-Arch=${Arch}WithTests=${WithTests}"/${BUILD_ID}
 #cp glRender*.zip ~/glRender/builds/${NODE_NAME}/"build-Arch=${Arch}WithTests=${WithTests}"/${BUILD_ID}	
 
-PUBLIC_DIR=~/public
-PROJECT_DIR=glRender
-BUILDS_DIR=${PUBLIC_DIR}/${PROJECT_DIR}/builds
-HEAD_SHA=$(git rev-parse --short HEAD)
+# PUBLIC_DIR=~/public
+# PROJECT_DIR=glRender
+# BUILDS_DIR=${PUBLIC_DIR}/${PROJECT_DIR}/builds
+# HEAD_SHA=$(git rev-parse --short HEAD)
 
-mkdir -p ${BUILDS_DIR}/${label}/${HEAD_SHA}/${BUILD_ID}/
-rm -rf ${BUILDS_DIR}/${label}/${HEAD_SHA}/${BUILD_ID}/*
-cp -r glRender*.* ${BUILDS_DIR}/${label}/${HEAD_SHA}/${BUILD_ID}/
+# mkdir -p ${BUILDS_DIR}/${label}/${HEAD_SHA}/${BUILD_ID}/
+# rm -rf ${BUILDS_DIR}/${label}/${HEAD_SHA}/${BUILD_ID}/*
+# cp -r glRender*.* ${BUILDS_DIR}/${label}/${HEAD_SHA}/${BUILD_ID}/
 
-mkdir -p ${BUILDS_DIR}/${label}/latest/
-rm -rf ${BUILDS_DIR}/${label}/latest/*
-cp -r glRender*.* ${BUILDS_DIR}/${label}/latest/
-echo ${HEAD_SHA} > ${BUILDS_DIR}/${label}/latest/COMMIT_SHA.txt
+# mkdir -p ${BUILDS_DIR}/${label}/latest/
+# rm -rf ${BUILDS_DIR}/${label}/latest/*
+# cp -r glRender*.* ${BUILDS_DIR}/${label}/latest/
+# echo ${HEAD_SHA} > ${BUILDS_DIR}/${label}/latest/COMMIT_SHA.txt
+
+if [[ "${GLRENDER_DEPLOY_PACKAGES}" == "ON" ]]; then
+    PUBLIC_DIR=public
+    PROJECT_DIR=glRender
+    HEAD_SHA=$(git rev-parse --short HEAD)
+    BUILD_DIR=${PROJECT_DIR}/${HEAD_SHA}/${GLRENDER_SLAVE_NAME}
+    PACKAGES_DIR=${BUILD_DIR}/packages/
+    DOCS_DIR=${BUILD_DIR}/docs/
+    LATEST_BUILD_LINK=${PROJECT_DIR}/latest
+    mkdir -p ${PACKAGES_DIR}
+    rm -rf ${LATEST_BUILD_LINK}
+    cd ${PROJECT_DIR}
+        ln -s ${HEAD_SHA} latest
+    cd ..
+    echo ${HEAD_SHA} > ${LATEST_BUILD_LINK}/COMMIT_SHA.txt
+    echo ${HEAD_SHA} > ${LATEST_BUILD_LINK}/${GLRENDER_SLAVE_NAME}/COMMIT_SHA.txt
+    echo ${HEAD_SHA} > ${LATEST_BUILD_LINK}/${GLRENDER_SLAVE_NAME}/packages/COMMIT_SHA.txt
+    
+    cp *glRender*.* ${PACKAGES_DIR}
+    scp -r ${PROJECT_DIR} jenkins@83.220.170.171:~/${PUBLIC_DIR}
+    rm -rf ${PROJECT_DIR}
+fi
