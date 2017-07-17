d5d3d5e306874e9cb6e4116b671981d23bf11fbc
Лизин Александр Сергеевич <lizin_as@tomsk.lemz.ru>
2017-07-17 11:17:57 +0700

    Fix comments position for constructors of PerspectiveCamera and OrthographicCamera

--- lib/include/orthographicCamera.hpp
+++ lib/include/orthographicCamera.hpp
@@ -7,15 +7,15 @@
 namespace glRender
 {
 
-/**
- * @param width The width of viewport
- * @param aspectRatio The ratio of width and height of viewport
- * @param nearPlane The distance to near plane in normalized units
- * @param farPlane The distance to far plane in normalized units
- */
 class OrthographicCamera : public Camera
 {
 public:
+    /**
+     * @param width The width of viewport in normalized units
+     * @param aspectRatio The ratio of width and height of viewport
+     * @param nearPlane The distance to near plane in normalized units
+     * @param farPlane The distance to far plane in normalized units
+     */
     OrthographicCamera(float width, float aspectRatio, float nearPlane, float farPlane);
     void calculateProjectionMatrix() override;
     void init();  /**< Method for avoid call calculateProjectionMatrix in constructor (virtual method)*/
--- lib/include/perspectiveCamera.hpp
+++ lib/include/perspectiveCamera.hpp
@@ -8,15 +8,15 @@
 namespace glRender
 {
 
-/**
- * @param fieldOfView Field of view in degrees
- * @param aspectRatio The ratio of width and height of viewport
- * @param nearPlane The distance to near plane in normalized units
- * @param farPlane The distance to far plane in normalized units
- */
 class PerspectiveCamera : public Camera
 {
 public:
+    /**
+     * @param fieldOfView Field of view in degrees
+     * @param aspectRatio The ratio of width and height of viewport
+     * @param nearPlane The distance to near plane in normalized units
+     * @param farPlane The distance to far plane in normalized units
+     */
     PerspectiveCamera(float fieldOfView, float aspectRatio, float nearPlane, float farPlane);
     void calculateProjectionMatrix() override;
     void init();  /**< Method for avoid call calculateProjectionMatrix in constructor (virtual method)*/
